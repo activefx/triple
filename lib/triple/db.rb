28aa14b4989c180aaf2370f75f9272183b4d8be4
Matt Solt <mattsolt@gmail.com>
2018-08-01 11:46:48 -0400

    enforce namespace naming requirements for db models

--- lib/triple/db.rb
+++ lib/triple/db.rb
@@ -1,11 +1,19 @@
 module Triple
   class DB
 
-    attr_reader :database, :namespace
+    attr_reader :database
 
     def initialize(**options)
       @database = options.fetch(:database) { Triple.default_database }
       @namespace = options.fetch(:namespace) { nil }
+    end
+
+    def namespace
+      if @namespace
+        match = /\A([A-Z][a-z]*([A-Z][a-zA-Z]*)?)\z/.match(@namespace.to_s)
+        match ? match.to_s : raise("Invalid namespace provided: " +
+          "please use CamelCase for the namespace class or module name")
+      end
     end
 
   end
--- spec/triple/db_spec.rb
+++ spec/triple/db_spec.rb
@@ -18,4 +18,20 @@ RSpec.describe Triple::DB do
 
   end
 
+  context "#namespace" do
+
+    it "is always retrieved as a string" do
+      instance = described_class.new namespace: Triple
+      expect(instance.namespace).to eq 'Triple'
+    end
+
+    it "require a valid Ruby class or module name" do
+      ['triple', 'SOME_CLASS', 'aClass', 'Triple_'].each do |name|
+        instance = described_class.new namespace: name
+        expect{ instance.namespace }.to raise_error StandardError
+      end
+    end
+
+  end
+
 end
