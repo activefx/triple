ede2c7157a203742a0f8c61e17e3e0b6a60dcba7
Matt Solt <mattsolt@gmail.com>
2018-08-02 19:01:53 -0400

    spec data models

---
+++ spec/triple/models/numeric_value_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+RSpec.describe 'NumericValue', type: :model do
+
+  let(:model) { 'NumericValue' }
+
+  let(:namespace) { 'Testing' }
+
+  let(:klass) { "#{namespace}::#{model}".constantize }
+
+  let(:triple) do
+    Triple.new namespace: namespace, database: data('models.db')
+  end
+
+  before(:each) do
+    ActiveRecord::Schema.verbose = false
+    File.delete(data('models.db')) if File.exist?(data('models.db'))
+    triple.setup
+  end
+
+  after(:each) do
+    triple.teardown
+    File.delete(data('models.db')) if File.exist?(data('models.db'))
+    ActiveRecord::Schema.verbose = true
+  end
+
+  context "relationships" do
+
+    subject { klass.new }
+
+    it "have one triples" do
+      is_expected.to have_one(:triple)
+    end
+
+  end
+
+end
---
+++ spec/triple/models/triple_spec.rb
@@ -0,0 +1,49 @@
+require 'spec_helper'
+
+RSpec.describe 'Triple', type: :model do
+
+  let(:model) { 'Triple' }
+
+  let(:namespace) { 'Testing' }
+
+  let(:klass) { "#{namespace}::#{model}".constantize }
+
+  let(:triple) do
+    Triple.new namespace: namespace, database: data('models.db')
+  end
+
+  before(:each) do
+    ActiveRecord::Schema.verbose = false
+    File.delete(data('models.db')) if File.exist?(data('models.db'))
+    triple.setup
+  end
+
+  after(:each) do
+    triple.teardown
+    File.delete(data('models.db')) if File.exist?(data('models.db'))
+    ActiveRecord::Schema.verbose = true
+  end
+
+  context "relationships" do
+
+    subject { klass.new }
+
+    it "belongs to source" do
+      is_expected.to belong_to(:source).class_name("#{namespace}::Source").dependent(:destroy)
+    end
+
+    it "belongs to entity" do
+      is_expected.to belong_to(:entity).class_name("#{namespace}::Entity").dependent(:destroy)
+    end
+
+    it "belongs to concept" do
+      is_expected.to belong_to(:concept).class_name("#{namespace}::Concept").dependent(:destroy)
+    end
+
+    it "belongs to value" do
+      is_expected.to belong_to(:value).dependent(:destroy)
+    end
+
+  end
+
+end
